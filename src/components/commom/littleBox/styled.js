75b4f4619dc0087ab1c392d0d4066d17ade6b2e7
Vinícius <viniciusotonimsv@gmail.com>
2021-12-03 15:39:50 -0200

    tela de selecionar horarios do filme feita (apenas o front)

---
+++ src/components/commom/littleBox/styled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+
+const StyledLittleBox = styled.div `
+
+    background-color: #AA0A0A;
+    border-radius: 2%;
+    color: #FFFFFF;
+    padding: 1.2em;
+
+    .language {
+        font-family: share-tech;
+        font-size: 1.7em;
+    }
+
+    .schedule {
+        font-size: 2.4em;
+        font-family: share-tech;
+    }
+`
+
+
+export { StyledLittleBox }
